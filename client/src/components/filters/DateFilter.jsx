dc025d5d972dc78508b6cf2a4873c4f1b3696d94
EC2 Default User <jj534@cornell.edu>
2020-03-21 13:01:13 +0000

    fixed readonly to react

--- client/src/components/filters/DateFilter.jsx
+++ client/src/components/filters/DateFilter.jsx
@@ -47,7 +47,7 @@ const DateFilter = ({ name, placeholder }) => {
   return (
     <Container>
       <StyledPicker
-        readonly='readonly'
+        readOnly={true}
         value={date ? undefined : placeholder}
         selected={date}
         onChange={(newDate) => setDate(newDate)}
