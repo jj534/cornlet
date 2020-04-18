bb7df2fb5bb2fef61c34329ee3e1bb5643451c03
EC2 Default User <jj534@cornell.edu>
2020-04-18 06:07:24 +0000

    line space fix

--- client/src/pages/listing/Listing/index.jsx
+++ client/src/pages/listing/Listing/index.jsx
@@ -117,7 +117,6 @@ const Listing = ({ listing }) => {
                     <Badge
                       color='primary'
                       inverted
-                      
                     >Sold
                     </Badge>
                   )}
