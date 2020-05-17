69a2935fc85f3666b085020e6dc9758fcd263b75
Jae Joo <jae@Jaes-MacBook-Air.local>
2020-05-17 17:11:53 +0900

    home listing loading dots animation

--- client/src/pages/home/Listings.jsx
+++ client/src/pages/home/Listings.jsx
@@ -6,19 +6,34 @@ import log from 'src/util/log';
 import DynCardList from 'src/containers/DynCardList';
 import useRouter from 'src/util/hooks/useRouter';
 import { useSelector } from 'react-redux';
+import LoadingDots from 'src/components/displays/LoadingDots';
+
+export const Center = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 const Listings = () => {
   const router = useRouter();
   const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const connector = router.location.search ? '&' : '?';
     api.get(`/listing${router.location.search}${connector}active=true`)
-      .then((res) => setListings(res.data))
+      .then((res) => {
+        setListings(res.data);
+        setLoading(false);
+      })
       .catch((e) => {
         log('ERROR get listings at home', e);
+        setLoading(false);
       });
   }, [router]);
+
+  if (loading) return <Center><LoadingDots /></Center>;
 
   return (
     <DynCardList>
