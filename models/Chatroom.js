8c7c4e16df0236675ced73c3679351af3a610104
Jae Joo <jae@Jaes-MacBook-Air.local>
2020-05-24 23:31:11 +0900

    chatroom schema init

---
+++ models/Chatroom.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const { Schema } = mongoose;
+
+const chatroomSchema = Schema({
+  users: {
+    type: [{
+      uid: {
+        type: String,
+        requried: true,
+      },
+      seen: {
+        type: Boolean,
+        default: false,
+      }
+    }],
+    required: true,
+  },
+  listing: {
+    type: Schema.Types.ObjectId,
+    ref: 'Listing',
+    required: true,
+  },
+  msgs: {
+    type: [{
+      type: {  // txt || img
+        type: String,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      uid: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: new Date
+      }
+    }],
+    default: [],
+  }
+}, {
+  timestamps: true,
+});
+
+chatroomSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Chatroom', chatroomSchema);
