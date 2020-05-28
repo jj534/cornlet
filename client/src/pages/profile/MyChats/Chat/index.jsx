d861178b4d402dae8a4a5ec251fbaa425f4698cc
Jae Joo <jae@Jaes-MacBook-Air.local>
2020-05-28 21:03:46 +0900

    chatlist basic logic

--- client/src/pages/profile/MyChats/Chat/index.jsx
+++ client/src/pages/profile/MyChats/Chat/index.jsx
@@ -3,17 +3,29 @@ import styled from 'styled-components';
 import MainHeader from 'src/components/headers/MainHeader';
 import Navbar from 'src/components/headers/Navbar';
 import ChatroomList from '../ChatroomList';
+import { useSelector } from 'react-redux';
+import Body from 'src/components/fonts/Body';
 
 const Container = styled.div`
 
 `;
 
+export const Center = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Chat = () => {
+  const chatrooms = useSelector((state) => state.chatrooms);
+
   return (
     <Container>
       <MainHeader />
       <Navbar />
-      <ChatroomList />
+      {chatrooms.length === 0
+        ? <Center><Body>No chats yet!</Body></Center>
+        : <ChatroomList />
+      }
     </Container>
   )
 };
--- client/src/pages/profile/MyChats/ChatroomList/index.jsx
+++ client/src/pages/profile/MyChats/ChatroomList/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import ListElt from './ListElt';
 
 const Container = styled.div`
 
 `;
 
 const ChatroomList = () => {
+  const chatrooms = useSelector((state) => state.chatrooms);
   
   return (
     <Container>
-        ChatroomList
+      {chatrooms.map((chatroom) => (
+        <ListElt key={chatroom._id} chatroom={chatroom} />
+      ))}
     </Container>
   )
 };
