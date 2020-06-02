1b0ddcaa6f6df15a8e34698ef16fff4e347731fc
Jae Joo <jae@Jaes-MacBook-Air.local>
2020-06-02 11:38:31 +0900

    invalid date fix attempt

--- client/src/util/helpers/formatDate.js
+++ client/src/util/helpers/formatDate.js
@@ -2,11 +2,11 @@ import moment from 'moment';
 
 const formatDate = (date, isText) => {
   const d = new Date(date);
-  const dateString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
   if (isText) {
-    return moment(dateString).format('MMMM D');
+    return moment(d).format('MMMM D');
   }
   else {
+    const dateString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
     return dateString
   }
 }
--- client/src/util/helpers/getDateString.js
+++ client/src/util/helpers/getDateString.js
@@ -2,7 +2,7 @@ import formatDate from 'src/util/helpers/formatDate';
 
 const getDateString = (listing) => {
   const { start, end } = listing;
-  return `${formatDate(start, true)} ~ ${formatDate(end, true)}, ${new Date(end).getFullYear()}`;
+  return `${formatDate(new Date(start), true)} ~ ${formatDate(new Date(end), true)}, ${new Date(end).getFullYear()}`;
 }
 
 export default getDateString;
