a37dc51da4594eb3b558c0dc552218da646cff16
Jae Joo <jj534@cornell.edu>
2020-06-11 17:41:57 +0900

    updated deployed socket client connection path

--- client/src/util/socket.js
+++ client/src/util/socket.js
@@ -1,6 +1,11 @@
 import io from "socket.io-client";
 
-const PORT = process.env.PORT || 8081
-const socket = io.connect(`http://localhost:${PORT}`);
+const URL = process.env.NODE_ENV === 'development'
+	? 'http://localhost:8081'
+	: process.env.REACT_APP_ENV === 'release'
+	? 'https://cornlet-release.herokuapp.com'
+	: 'https://www.cornlet.com'
+	
+const socket = io.connect(URL);
 
 export default socket;
