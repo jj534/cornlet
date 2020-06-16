29de8b9681475047b3d77c1a2aa68ad81732ec2b
Jae Joo <jj534@cornell.edu>
2020-06-16 12:57:13 +0900

    show year if year is diff

--- client/src/util/helpers/getDateString.js
+++ client/src/util/helpers/getDateString.js
@@ -2,7 +2,10 @@ import formatDate from 'src/util/helpers/formatDate';
 
 const getDateString = (listing) => {
   const { start, end } = listing;
-  return `${formatDate(new Date(start), true)} ~ ${formatDate(new Date(end), true)}, ${new Date(end).getFullYear()}`;
+  const startYear = new Date(start).getFullYear() !== new Date(end).getFullYear()
+    ? `, ${new Date(start).getFullYear()}`
+    : ''
+  return `${formatDate(new Date(start), true)}${startYear} ~ ${formatDate(new Date(end), true)}, ${new Date(end).getFullYear()}`;
 }
 
 export default getDateString;
