01faf4763d3962d1c8165c7030c3c712156d77e8
Jae Joo <jj534@cornell.edu>
2020-06-20 19:33:41 +0900

    back to top button

--- client/src/components/core/ScrollToTop.jsx
+++ client/src/components/core/ScrollToTop.jsx
@@ -1,12 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import useRouter from 'src/util/hooks/useRouter';
+import { ReactComponent as UpRaw } from 'src/assets/svgs/up.svg';
+import useIsDesktop from 'src/util/hooks/useIsDesktop';
+
+export const Container = styled.div`
+  border-radius: 50%;
+  background: ${props => props.theme.primary};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .2);
+
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+  z-index: 10;
+  width: 3rem;
+  height: 3rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+`;
+
+const UpSVG = styled(UpRaw)`
+  height: 1.2rem;
+  width: 1.2rem;
+  fill: white;
+`
 
 const ScrollToTop = () => {
+  // scroll to top on route change
   const router = useRouter();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [router.location]);
-  return <div />;
+
+  // for mobile, if not currently at top
+  // render back to top button
+  const isDesktop = useIsDesktop();
+  const [isAtTop, setIsAtTop] = useState(true);
+  window.onscroll = function() {
+    if(window.pageYOffset === 0) {
+      setIsAtTop(true);
+    }
+    else {
+      setIsAtTop(false);
+    }
+  };
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  if (isDesktop || isAtTop) return <div />;
+
+  return (
+    <Container onClick={handleClick}>
+      <UpSVG />
+    </Container>
+  );
 };
 
 export default ScrollToTop;
