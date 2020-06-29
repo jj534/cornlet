aacc899732d21c3222aa52e176dd91ba1c6b2fb5
Jae Joo <jj534@cornell.edu>
2020-06-29 14:56:14 +0900

    auto dispatch authing_set on signin helper function call

--- client/src/components/core/PrivateRoute.jsx
+++ client/src/components/core/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Route } from 'react-router-dom';
 import MainHeader from '../headers/MainHeader';
+import signin from 'src/util/helpers/signin';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = useSelector((state) => state.user);
@@ -9,11 +10,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   const dispatch = useDispatch();
 
   if (!user && !authing) {
-    window.open('http://localhost:8081/api/auth/google', '_self');
-    dispatch({
-      type: 'AUTHING_SET',
-      payload: true,
-    });
+    signin();
     return <div />;
   }
 
--- client/src/util/helpers/signin.js
+++ client/src/util/helpers/signin.js
@@ -1,5 +1,11 @@
+import store from 'src/redux/store';
+
 const signin = () => {
   window.open('http://localhost:8081/api/auth/google', '_self');
+  store.dispatch({
+    type: 'AUTHING_SET',
+    payload: true,
+  });
 }
 
 export default signin;
