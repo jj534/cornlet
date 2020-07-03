f31c4b434d52d20595f254d73d6993a29453d78c
Jae Joo <jj534@cornell.edu>
2020-07-03 19:35:44 +0900

    fixed signin helper typo

--- client/src/util/helpers/signin.js
+++ client/src/util/helpers/signin.js
@@ -1,7 +1,7 @@
 import store from 'src/redux/store';
 
 const signin = (options) => {
-  window.open(`${process.env.REACT_APP_CLIENT_DOMAIN}/api/auth/google', '_self`);
+  window.open(`${process.env.REACT_APP_CLIENT_DOMAIN}/api/auth/google`, '_self');
   store.dispatch({
     type: 'AUTHING_SET',
     payload: true,
