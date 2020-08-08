e493197c0f3e5f161e99b2d3764f95bee8bc4c2b
Jae Joo <jj534@cornell.edu>
2020-08-09 07:48:34 +0900

    pagination: hid prev button, document title: changed to sublets

--- client/src/components/buttons/PaginationBtns.jsx
+++ client/src/components/buttons/PaginationBtns.jsx
@@ -10,27 +10,6 @@ const Container = styled.div`
   margin: 2rem 0;
 `;
 
-const Page = styled.button`
-  margin: 0 .2rem;
-  height: 2rem;
-  width: 2rem;
-  border-radius: 50%;
-  background: inherit;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  // selected
-  background: ${props => props.selected ? 'rgba(0, 0, 0, .7)' : ''};
-  color: ${props => props.selected ? 'white' : ''};
-`;
-
-const Dots = styled.div`
-  transform: translateY(0.15em);
-  margin: 0 .2rem;
-`
-
 const PaginationBtns = ({ totalPages, page }) => {
   const pages = [];
   for (let i = 1; i <= totalPages; i++) {
@@ -39,15 +18,12 @@ const PaginationBtns = ({ totalPages, page }) => {
 
   const router = useRouter();
   const handleChange = (e, v) => {
-    console.log('v', v);
-    console.log('e', e);
     router.updateQuery({ page: v });
   };
-  // const page = router.query.page || 1;
 
   return (
     <Container>
-        <Pagination count={totalPages} page={page} onChange={handleChange} />
+        <Pagination count={totalPages} page={page} onChange={handleChange} hidePrevButton />
     </Container>
   )
 };
